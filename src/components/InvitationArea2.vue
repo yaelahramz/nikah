ff618a1b38ef05081e00d5ec7b02f648ae27eb57
yaelahramz <taraprojectid@gmail.com>
2024-05-24 23:24:34 +0900

    arti surat

--- src/components/InvitationArea2.vue
+++ src/components/InvitationArea2.vue
@@ -7,6 +7,7 @@
       وَمِنْ اٰيٰتِهٖٓ اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ
 اَزْوَاجًا لِّتَسْكُنُوْٓا اِلَيْهَا وَجَعَلَ بَيْنَكُمْ
 مَّوَدَّةً وَّرَحْمَةًۗ اِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ يَّتَفَكَّرُوْنَ ۝٢١<br />
+Di antara tanda-tanda (kebesaran)-Nya ialah bahwa Dia menciptakan pasangan-pasangan untukmu dari (jenis) dirimu sendiri agar kamu merasa tenteram kepadanya. Dia menjadikan di antaramu rasa cinta dan kasih sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.<br />
     </div>
 
     <Divider3 :padding-bottom="0" padding-right="24" padding-left="24"/>
