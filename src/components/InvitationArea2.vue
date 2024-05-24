92d6690f4030cab063693c5bc37fb43423b7eb17
yaelahramz <taraprojectid@gmail.com>
2024-05-24 23:20:09 +0900

    surat

--- src/components/InvitationArea2.vue
+++ src/components/InvitationArea2.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="invitation_area">
     <p class="header _big_text _green">
-      <strong>"우리 손 잡을까요?"</strong><br />
+      <strong>"QS Ar Rum 21"</strong><br />
     </p>
     <div class="desc _large_text _gray">
-      각자의 삶을 열심히 살아온 저희가,<br />
-      이제는 손을 잡고 함께 나란히 걸어가려 합니다.<br />
-      사랑한다는 말이 어울리는 사람과 함께<br />
-      오래도록 있는 힘을 다해 행복해지겠습니다.<br />
-      조용하지만 기쁨이 가득한 결혼 소식을<br />
-      이렇게 알려드립니다.
+      وَمِنْ اٰيٰتِهٖٓ اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ
+اَزْوَاجًا لِّتَسْكُنُوْٓا اِلَيْهَا وَجَعَلَ بَيْنَكُمْ
+مَّوَدَّةً وَّرَحْمَةًۗ اِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ يَّتَفَكَّرُوْنَ ۝٢١<br />
     </div>
 
     <Divider3 :padding-bottom="0" padding-right="24" padding-left="24"/>
