06395604139e1349788f3f9968eb93dfc38bc86e
yaelahramz <taraprojectid@gmail.com>
2024-05-25 00:01:02 +0900

    center edit

--- src/components/InvitationArea2.vue
+++ src/components/InvitationArea2.vue
@@ -65,7 +65,7 @@ export default {
     .male {
       font-size: 12.5px;
       margin-left: 26px;
-      margin-right: 26px;
+      margin-right: 43px;
     }
     .female {
       font-size: 12.5px;
