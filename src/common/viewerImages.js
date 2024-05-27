1595a04afb6666ed45cd8ab5c34a62b7310c3992
yaelahramz <taraprojectid@gmail.com>
2024-05-27 21:53:47 +0900

    update photo

--- src/common/viewerImages.js
+++ src/common/viewerImages.js
@@ -1,8 +1,5 @@
 export default {
 	images : [
-		"my-wedding/gallery/0_h.jpg",
-		"my-wedding/gallery/1_h.jpg",
-		"my-wedding/gallery/2_h.jpg",
 		"my-wedding/gallery/3.jpg",
 		"my-wedding/gallery/4.jpg",
 		"my-wedding/gallery/5.jpg",
@@ -12,18 +9,6 @@ export default {
 		"my-wedding/gallery/9.jpg",
 		"my-wedding/gallery/10.jpg",
 		"my-wedding/gallery/11.jpg",
-		"my-wedding/gallery/12.jpg",
-		"my-wedding/gallery/13.jpg",
-		"my-wedding/gallery/14.jpg",
-		"my-wedding/gallery/15_h.jpg",
-		"my-wedding/gallery/16_h.jpg",
-		"my-wedding/gallery/17_h.jpg",
-		"my-wedding/gallery/18.jpg",
-		"my-wedding/gallery/19.jpg",
-		"my-wedding/gallery/20.jpg",
-		"my-wedding/gallery/21.jpg",
-		"my-wedding/gallery/22.jpg",
-		"my-wedding/gallery/23.jpg",
 	]
 
 }
