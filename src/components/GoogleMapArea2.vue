f8e13a7faeb063f7b980487720f774d43e1a0caa
yaelahramz <taraprojectid@gmail.com>
2024-05-28 21:32:32 +0900

    update maps

--- src/components/GoogleMapArea2.vue
+++ src/components/GoogleMapArea2.vue
@@ -38,51 +38,16 @@
     </iframe>
 
     <div class="util_btn_icon_area">
-      <a :href="navermapUrl">
-        <img class="util_btn_icon" src="../assets/images/new/map/gojek.png"/>
-      </a>
-      <a :href="kakaoTaxiUrl">
-        <img class="util_btn_icon" src="../assets/images/new/map/grab.png"/>
-      </a>
       <a :href="tmapUrl">
         <img class="util_btn_icon" src="../assets/images/new/map/maps.png"/>
       </a>
     </div>
 
     <div class="map_desc_area _tiny_text _gray">
-      <p class="_large_text _green"><b>자가용</b></p>
       <Divider3 :padding-bottom="0" padding-right="44"/>
-      <strong class="_small_text _black">경부고속도로</strong><br>
-      양재IC 양재 방면으로 오른쪽 방향<br>
-      > 개포3, 4단지 삼거리(대치역 방면 좌회전)<br>
-      > 은마 아파트 > 대치우성 아파트 좌회전<br>
-      > 노블발렌티 대치점<br>
-      <br>
-      <strong class="_small_text _black">서울 서부, 강북지역</strong><br>
-      영동대교 남단 '경기고교사거리' 방면 > 봉은사역<br>
-      > 삼성역 직진 > 노블발렌티 대치점<br>
-      <br>
-      <strong class="_small_text _black">서울 동남부 지역</strong><br>
-      2호선 삼성역(무역센터)교차로 > 학여울역(SETEC)방향<br>
-      우회전 > 노블발렌티 대치점
     </div>
 
-    <div class="map_desc_area _tiny_text _gray">
-      <p class="_large_text _green"><b>지하철</b></p>
-      <Divider3 :padding-bottom="0" padding-right="44"/>
-      <strong class="_small_text _black">2호선 삼성역 3번 출구 30m 전방에서 셔틀버스 운행</strong><br>
-      셔틀버스 수시운행, 도보 이용시 7분 소요
-    </div>
 
-    <div class="map_desc_area _tiny_text _gray">
-      <p class="_large_text _green"><b>버스</b></p>
-      <Divider3 :padding-bottom="0" padding-right="44"/>
-      <strong class="_small_text _black">휘문고, 대치 2동 주민센터 하차</strong><br>
-      간선: 342, 401<br>
-      지선: 4318, 4319<br>
-      일반: 11-3, 917<br>
-      직행: 500-2, 9407, 9507, 9607
-    </div>
   </div>
 </template>
 
